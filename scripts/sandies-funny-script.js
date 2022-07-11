ba452800bdf3de41a3e28d1a536744deaf004dd5
rRubedog <89792821+rRubedog@users.noreply.github.com>
2022-07-11 09:45:07 -0600

    Added an Easter egg

---
+++ scripts/sandies-funny-script.js
@@ -0,0 +1,9 @@
+function column() {
+    document.getElementById("sandwich-section-wrapper").style.flexDirection = "column-reverse";
+    document.getElementById("bread-header").innerText = "Oops";
+    document.querySelector("#link1").innerText = "Y'all's";
+    document.querySelector("#link2").innerText = "made an";
+    document.querySelector("#link3").innerText = "oopsie";
+    document.querySelector("#p1").innerText = "You like my sandies?";
+    document.querySelector("#p2").innerText = "Btw you should try reloading the page now";
+}
